926b8f455cd8ce403d101f7969715aa7af6fb1da
Sushant Kamble <sushant@scalextechnologies.com>
2019-03-14 16:24:58 +0530

    Token Generation logic added

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { Root } from 'native-base'
 import { createStore, compose } from 'redux'
 import reducer from './src/store/reducers'
 import { Provider } from 'react-redux'
+import firebase from 'react-native-firebase';
 
 export default class App extends Component {
 
@@ -25,36 +26,75 @@ export default class App extends Component {
 
     }
 
-  retrieveData = async () => {
-      try {
-      //Check if previous state exists
-          const value = await AsyncStorage.getItem('reduxState');
-          if (value) {
-              // We have state!!
-              this.store = createStore(reducer, JSON.parse(value), this.composeEnhancers())
-          } else {
-              //Create a new store with initial data
-              this.store = createStore(reducer, this.composeEnhancers())
-          }
-          //persist data each time when an action is called
-          this.store.subscribe(() => {
-              AsyncStorage.setItem('reduxState', JSON.stringify(this.store.getState()))
-          })
-          //UI can be loaded now
-          this.setState({ dataLoaded: true })
-      } catch (error) {
-      // Error retrieving data
-      }
-  }
-  render() {
-      return (
-          this.state.dataLoaded ?
-              <Root>
-                  <Provider store={this.store}>
-                      <Navigator />
-                  </Provider>
-              </Root>
-              : null
-      )
-  }
+    async componentDidMount() {
+        this.checkPermission();
+    }
+    //1 : Check for Push Notification Permissions
+    async checkPermission() {
+        const enabled = await firebase.messaging().hasPermission();
+        if (enabled) {
+            this.getToken();
+        } else {
+            this.requestPermission();
+        }
+    }
+
+    //2 : Request for Permission if permission not available
+    async requestPermission() {
+        try {
+            await firebase.messaging().requestPermission();
+            // User has authorised
+            this.getToken();
+        } catch (error) {
+            // User has rejected permissions
+            console.log('permission rejected');
+        }
+    }
+
+    //3 : Get the device token and store it in Async storage
+    async getToken() {
+        let fcmToken = await AsyncStorage.getItem('fcmToken');
+        if (!fcmToken) {
+            fcmToken = await firebase.messaging().getToken();
+            
+            if (fcmToken) {
+                // user has a device token
+                await AsyncStorage.setItem('fcmToken', fcmToken);
+            }
+        }
+        console.log('FCM Token : ')
+        console.log(fcmToken)
+    }
+    retrieveData = async () => {
+        try {
+            //Check if previous state exists
+            const value = await AsyncStorage.getItem('reduxState');
+            if (value) {
+                // We have state!!
+                this.store = createStore(reducer, JSON.parse(value), this.composeEnhancers())
+            } else {
+                //Create a new store with initial data
+                this.store = createStore(reducer, this.composeEnhancers())
+            }
+            //persist data each time when an action is called
+            this.store.subscribe(() => {
+                AsyncStorage.setItem('reduxState', JSON.stringify(this.store.getState()))
+            })
+            //UI can be loaded now
+            this.setState({ dataLoaded: true })
+        } catch (error) {
+            // Error retrieving data
+        }
+    }
+    render() {
+        return (
+            this.state.dataLoaded ?
+                <Root>
+                    <Provider store={this.store}>
+                        <Navigator />
+                    </Provider>
+                </Root>
+                : null
+        )
+    }
 }
