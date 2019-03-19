9b4afac1ee7802f846be7a511878f7f8da0e4902
Sushant Kamble <sushant@scalextechnologies.com>
2019-03-19 12:57:17 +0530

    linked createPost handler to global header

--- src/containers/CreatePost/index.js
+++ src/containers/CreatePost/index.js
@@ -23,7 +23,7 @@ import { connect } from 'react-redux'
 /* Assets */
 import image from '../../assets/image.jpg'
 /* Services */
-import {create_post} from '../../services/post'
+import { create_post } from '../../services/post'
 /* Components */
 import VisibilityModal from '../VisibilityModal'
 import LoadingModal from '../LoadingModal'
@@ -31,15 +31,31 @@ class CreatePost extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state={
-            visibilityModal:false,
-            visibilitySelection:'',
-            text:'',
-            isLoading:false
+        this.state = {
+            visibilityModal: false,
+            visibilitySelection: '',
+            text: '',
+            isLoading: false
         }
 
     }
+    static navigationOptions = ({ navigation }) => {
+        return {
+            
+            headerRight: (
+                <Icon name='md-checkmark' style={
+                    {color: 'white',
+                        margin:20
+                    }
+                } onPress={navigation.getParam('postSubmitHandler')} />
+            ),
+            headerLeft: (
+                <React.Fragment/>
+            )
+        };
+    };
     componentDidMount() {
+        this.props.navigation.setParams({ postSubmitHandler: this.postSubmitHandler });
         this.backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
             this.goBack()
             return true
@@ -51,9 +67,9 @@ class CreatePost extends React.Component {
     }
 
     goBack = () => {
-        if(this.state.text===''){
+        if (this.state.text === '') {
             this.props.navigation.navigate('TabNavigator')
-        }else{
+        } else {
             Alert.alert(
                 'Are you sure?',
                 'Note will not be saved',
@@ -71,61 +87,61 @@ class CreatePost extends React.Component {
                 { cancelable: false },
             )
         }
-        
+
     }
-    postSubmitHandler=()=>{
-        if(this.state.text===''){
+    postSubmitHandler = () => {
+        if (this.state.text === '') {
             Toast.show({
                 text: 'Please write someting',
                 type: 'danger',
-                duration:3000
+                duration: 3000
             })
             return
         }
         const payload = {
             tenant_id: this.props.accountAlias,
-            associate_id:this.props.associate_id,
+            associate_id: this.props.associate_id,
             type: "post",
             message: this.state.text,
             privacy: this.state.visibilitySelection
         }
-        this.setState({isLoading:true})
+        this.setState({ isLoading: true })
         try {
-            create_post(payload).then(response=>{
+            create_post(payload).then(response => {
                 Toast.show({
                     text: response.data.message,
                     type: 'success',
-                    duration:3000
+                    duration: 3000
                 })
                 this.setState({
-                    isLoading:false,
-                    text:''
+                    isLoading: false,
+                    text: ''
                 })
-                this.props.navigation.navigate('Home',{refresh:true})
-                
-            }).catch((error)=>{
-                
+                this.props.navigation.navigate('Home', { refresh: true })
+
+            }).catch((error) => {
+
                 Toast.show({
                     text: error.response.data.code,
                     type: 'danger',
-                    duration:3000
+                    duration: 3000
                 })
-                this.setState({isLoading:false})
-                
-            })    
+                this.setState({ isLoading: false })
+
+            })
         } catch (error) {
             Toast.show({
                 text: 'No internet connection',
                 type: 'danger',
-                duration:3000
+                duration: 3000
             })
-            this.setState({isLoading:false})
+            this.setState({ isLoading: false })
         }
-        
+
     }
 
     render() {
-        const fontSize=15
+        const fontSize = 15
         return (
 
             <Container>
@@ -138,113 +154,107 @@ class CreatePost extends React.Component {
                         onLoadEnd={this.loadComponents}
                     >
 
-                        <View name='header' style={styles.headerContainer}>
-                            <Icon style={styles.header} />
-                            <H3 style={styles.headerText}>Leave a note</H3>
-                            <Icon name='md-checkmark' style={styles.header} onPress={this.postSubmitHandler} />
-                        </View>
-
                         <View name='content' style={styles.content}>
                             <View style={{
-                                flex:1,
-                                backgroundColor:'#fff',
-                                marginHorizontal:10,
-                                marginBottom:20,
-                                borderRadius:5,
-                                alignItems:'center'
+                                flex: 1,
+                                backgroundColor: '#fff',
+                                marginHorizontal: 10,
+                                marginBottom: 20,
+                                borderRadius: 5,
+                                alignItems: 'center'
                             }}>
                                 <TouchableOpacity
                                     style={{
-                                        backgroundColor:'#1c92c4',
-                                        flexDirection:'row',
-                                        alignItems:'center',
-                                        justifyContent:'center',
+                                        backgroundColor: '#1c92c4',
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
                                         paddingHorizontal: 10,
                                         paddingVertical: 5,
-                                        marginHorizontal:10,
+                                        marginHorizontal: 10,
                                         marginTop: 10,
-                                        borderRadius:5
+                                        borderRadius: 5
                                     }}
-                                    onPress={()=>this.setState({visibilityModal:true})}
+                                    onPress={() => this.setState({ visibilityModal: true })}
                                 >
-                                    <Icon name='md-eye' style={{fontSize:12,paddingHorizontal:5,color:'white'}} />
+                                    <Icon name='md-eye' style={{ fontSize: 12, paddingHorizontal: 5, color: 'white' }} />
                                     <Text style={styles.buttonText}>{this.state.visibilitySelection}</Text>
                                 </TouchableOpacity>
                                 <View style={{
-                                    backgroundColor:'#333',
-                                    height:1,
-                                    width:'90%',
+                                    backgroundColor: '#333',
+                                    height: 1,
+                                    width: '90%',
                                     marginVertical: 10
-                                }}/>
+                                }} />
                                 <TextInput
-                                    multiline = {true}
+                                    multiline={true}
                                     maxLength={255}
                                     placeholder='Write something here'
                                     scrollEnabled={true}
                                     style={{
-                                        
-                                        padding:20,
-                                        flex:1,
-                                        fontSize:20,
-                                        width:'100%',
-                                        textAlignVertical:'top'
-                                        
+
+                                        padding: 20,
+                                        flex: 1,
+                                        fontSize: 20,
+                                        width: '100%',
+                                        textAlignVertical: 'top'
+
                                     }}
                                     value={this.state.text}
-                                    onChangeText={(text)=>this.setState({text:text})}
+                                    onChangeText={(text) => this.setState({ text: text })}
                                 />
-                                
+
                                 <View style={{
-                                    backgroundColor:'#333',
-                                    height:1,
-                                    width:'70%',
+                                    backgroundColor: '#333',
+                                    height: 1,
+                                    width: '70%',
                                     marginVertical: 10
-                                }}/>
-                                
+                                }} />
+
                                 <View name='buttonContainer' style={{
-                                    marginHorizontal:5,
-                                    marginBottom:10,
-                                    flexDirection:'row',
-                                    alignItems:'center',
+                                    marginHorizontal: 5,
+                                    marginBottom: 10,
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
                                     justifyContent: 'center'
                                 }}>
-                                    <TouchableOpacity style={styles.button} onPress={()=>Toast.show({
+                                    <TouchableOpacity style={styles.button} onPress={() => Toast.show({
                                         text: 'coming soon!',
                                         type: 'success',
-                                        duration:3000
+                                        duration: 3000
                                     })}>
-                                        <Icon name='md-people' style={{fontSize:fontSize,paddingHorizontal:5,color:'white'}} />
-                                        <Text style={[styles.buttonText,{fontSize:fontSize}]}>Endorse</Text>
+                                        <Icon name='md-people' style={{ fontSize: fontSize, paddingHorizontal: 5, color: 'white' }} />
+                                        <Text style={[styles.buttonText, { fontSize: fontSize }]}>Endorse</Text>
                                     </TouchableOpacity>
-                                    <TouchableOpacity style={styles.button} onPress={()=>Toast.show({
+                                    <TouchableOpacity style={styles.button} onPress={() => Toast.show({
                                         text: 'coming soon!',
                                         type: 'success',
-                                        duration:3000
+                                        duration: 3000
                                     })}>
-                                        <Icon name='md-thumbs-up' style={{fontSize:fontSize,paddingHorizontal:5,color:'white'}} />
-                                        <Text style={[styles.buttonText,{fontSize:fontSize}]}>Gratitude</Text>
+                                        <Icon name='md-thumbs-up' style={{ fontSize: fontSize, paddingHorizontal: 5, color: 'white' }} />
+                                        <Text style={[styles.buttonText, { fontSize: fontSize }]}>Gratitude</Text>
                                     </TouchableOpacity>
-                                    
+
                                 </View>
                             </View>
-                            
+
                         </View>
                     </ImageBackground>
 
                 </Content>
-                
+
                 <VisibilityModal
                     enabled={this.state.visibilityModal}
                     data={[
-                        {icon:'md-globe',text:'Organization'},
-                        {icon:'md-people',text:'Project'},
-                        {icon:'md-person',text:'Private'}
+                        { icon: 'md-globe', text: 'Organization' },
+                        { icon: 'md-people', text: 'Project' },
+                        { icon: 'md-person', text: 'Private' }
                     ]}
-                    onChangeListener={(text)=>{
-                        this.setState({visibilitySelection:text})
+                    onChangeListener={(text) => {
+                        this.setState({ visibilitySelection: text })
                     }}
-                    visibilityDisableHandler={()=>{
-                        this.setState({visibilityModal:false})
+                    visibilityDisableHandler={() => {
+                        this.setState({ visibilityModal: false })
                     }}
                     state={this.state.visibilitySelection}
                 />
@@ -252,22 +262,22 @@ class CreatePost extends React.Component {
                     enabled={this.state.isLoading}
                 />
             </Container>
-            
+
         );
     }
 }
 
 const styles = StyleSheet.create({
-    button:{
-        flex:1,
-        backgroundColor:'#1c92c4',
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent:'center',
+    button: {
+        flex: 1,
+        backgroundColor: '#1c92c4',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
         paddingHorizontal: 10,
         paddingVertical: 5,
-        margin:5,
-        borderRadius:5
+        margin: 5,
+        borderRadius: 5
     },
     container: {
         flex: 1
@@ -294,20 +304,20 @@ const styles = StyleSheet.create({
         color: 'white',
         width: 20
     },
-    headerText:{
+    headerText: {
         color: 'white',
         textAlign: 'center'
     },
-    buttonText:{
+    buttonText: {
         color: 'white',
         textAlign: 'center'
-        
+
     }
 });
 
 const mapStateToProps = (state) => {
     return {
-        accountAlias:state.user.accountAlias,
+        accountAlias: state.user.accountAlias,
         associate_id: state.user.associate_id
 
     };
