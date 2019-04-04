dd23e0358d22e1a1129a07027670a6ea85e48e36
yuvraj-scalex <49190696+yuvraj-scalex@users.noreply.github.com>
2019-04-04 11:41:15 +0530

    Offline Feature Notice boilerplate

---
+++ src/components/OfflineNotice/index.js
@@ -0,0 +1,50 @@
+import React, { PureComponent } from "react";
+import { View, Text, Dimensions, StyleSheet, NetInfo } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
+function MiniOfflineSign() {
+    return (
+        <View style={styles.offlineContainer}>
+            <Text style={styles.offlineText}>No Internet Connection</Text>
+        </View>
+    );
+}
+
+export default class OfflineNotice extends PureComponent {
+   
+    state = {
+        isConnected: this.props.isConnected
+    }
+
+    componentDidMount() {
+        NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
+    }
+
+    componentWillUnmount() {
+        NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+    }
+
+    handleConnectivityChange = isConnected => {
+        this.setState({ isConnected: isConnected ? true : false });
+    };
+
+    render() {
+        if (!this.state.isConnected) {
+            return <MiniOfflineSign />;
+        }
+        return null;
+    }
+}
+
+const styles = StyleSheet.create({
+    offlineContainer: {
+        backgroundColor: '#b52424',
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        width
+    },
+    offlineText: { color: '#fff' }
+});
