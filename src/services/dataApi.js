7eef324f86250b304e4e4a3676e38c1604cfa924
Sushant Kamble <sushant@scalextechnologies.com>
2019-06-28 18:30:23 +0530

    Endpoints Migrated

--- src/services/dataApi.js
+++ src/services/dataApi.js
@@ -1,13 +1,10 @@
 import config from '../../config'
 import axios from 'axios'
 
-const url = config.url.dataApi
-const env = config.env.dev
-const type = config.type.data_apis
 export const save_answers = (
     payload, headers
 ) => {
-    const endpoint = url + "/" + env + "/" + type + "/" + config.api.save_answers
+    const endpoint = config.api.save_answers
     return axios.post(endpoint, 
         payload, headers
     )
