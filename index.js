5e312f6193ba2be058012499bd65ccd19fca8dcd
Sushant KambIe <sushant@scalextechnologies.com>
2019-11-09 19:25:16 +0530

    Added network debugger to monitor api calls

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ import App from './App';
 import {name as appName} from './app.json';
 /* Ignore yellow warnings */
 console.disableYellowBox = true
+/* Debug api calls */
+XMLHttpRequest = GLOBAL.originalXMLHttpRequest ? 
+  GLOBAL.originalXMLHttpRequest : GLOBAL.XMLHttpRequest;
+  
 AppRegistry.registerComponent(appName, () => App);
